730770475cc79c9f7ad1de77b48def57082c219b
Víctor Martínez <vito.marodriguez@gmail.com>
2024-07-10 11:52:10 -0600

    barra de progreso en la parte superior del viewport

--- src/context/experiencia.jsx
+++ src/context/experiencia.jsx
@@ -37,6 +37,7 @@ export function ExperienciaProvider({ children }) {
         try {
             const res = await fetch("")
             const data = await res.json()
+            console.log(data)
             // transformar a Item
             setProyectos(data)
             setLoading(false)
